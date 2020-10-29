cd89fcb090f9c0b9c6f38b0ef17a386bf9b871b0
HumanStudent <salesforce.kavmahve.task@gmail.com>
2020-10-29 22:28:09 +0200

    gitToken

--- testGithub.py
+++ testGithub.py
@@ -1,10 +1,12 @@
+import os
 from github import Github
 from pathlib import Path
 from dotenv import load_dotenv
 
-# env_path = Path('.') / '.varFileEnv'
-# load_dotenv(dotenv_path=env_path) 
-
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path) 
+gitToken = os.environ['GITTOKEN']
+g = Github(gitToken)
 # GITHUB_ACCESS_TOKEN
 # g = Github("HumanStudent", "hellohuman1")
 
