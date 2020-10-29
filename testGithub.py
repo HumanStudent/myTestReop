e8bdbf2d56b02cd56b85dd4f9bb97de5d4231dc2
HumanStudent <salesforce.kavmahve.task@gmail.com>
2020-10-29 22:31:01 +0200

    arrange code lines

--- testGithub.py
+++ testGithub.py
@@ -7,8 +7,6 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path) 
 gitToken = os.environ['GITTOKEN']
 g = Github(gitToken)
-# GITHUB_ACCESS_TOKEN
-# g = Github("HumanStudent", "hellohuman1")
 
 repos = g.get_user().get_repos()
 for repo in repos:
