f7c94b54473851180f5e1e62dd828e4349323252
HumanStudent <salesforce.kavmahve.task@gmail.com>
2020-10-30 19:15:24 +0200

    reqular push

--- testGithub.py
+++ testGithub.py
@@ -2,20 +2,6 @@ import os
 from github import Github
 from pathlib import Path
 from dotenv import load_dotenv
-from github_webhook import Webhook
-from flask import Flask, request, Response
-
-app = Flask(__name__)  # Standard Flask app
-webhook = Webhook(app) # Defines '/postreceive' endpoint
-
-@app.route("/")        # Standard Flask endpoint
-def hello_world():
-    return "Hello, World!"
-
-@webhook.hook()        # Defines a handler for the 'push' event
-def on_push(data):
-    print("Got push with: {0}".format(data))
-
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path) 
